279e89e1faaeae34a925ffac4adedcaf8f2d4823
OswaldoPerez <oswaldopem@gmail.com>
2026-07-27 12:38:02 -0600

    Fist commit
    This commit include
    *.gitignore for Unreal Engine
     *Cursor IDE ignores
     *Raider IDE ignores
     *VS & VC IDE Ignores
    *.gitattributes with LFS
    *.uproject

---
+++ Akbal/Source/Akbal/Akbal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Akbal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Akbal, "Akbal" );
